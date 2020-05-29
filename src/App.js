9f7671f6261350cc6eb4348ec989805d3f24c4a0
Sudip <sudipchavan100@gmail.com>
2020-05-29 19:52:34 +0100

    my project

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import FAQ from './FAQ';
+//import Faqs from "./faqs.json";
+
+function App () {
+  const [faqs, setfaqs] = useState([
+    {
+      question: "What vehicles are covered?",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      open: true
+    },
+    {
+      question: "Can anyone drive a vehicle covered by a business policy?",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      open: false
+    },
+    {
+      question: "You say you don't charge for specialist equipment, what does that mean?",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      open: false
+    },
+    {
+        question: "Can I change my cover level and add/remove vehicles?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        open: false
+      },
+      {
+        question: "What happens if my vehicle needs repairing and it's outside of business hours?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        open: false
+      }
+    
+  ]);
+
+  const toggleFAQ = index => {
+    setfaqs(faqs.map((faq, i) => {
+      if (i === index) {
+        faq.open = !faq.open
+      } else {
+        faq.open = false;
+      }
+
+      return faq;
+    }))
+  }
+
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="faqs">
+        
+        {faqs.map((faq, i) => (
+          
+          < FAQ faq={faq} index={i} toggleFAQ={toggleFAQ} />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header () {
+	return (
+		<header>
+			<h1>HAVE A QUESTION? WE CAN HELP</h1>
+			<h2></h2>
+		</header>
+	)
+}
+
+export default Header
